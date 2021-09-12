7741594bd9c147c7939d08ce0bb1ed9251c21fe7
Payload <anegreiross@outlook.com>
2021-09-12 13:16:20 -0500

    finished challenge and change currency type

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 
 from app import *
-from flask import jsonify
+from flask import (
+    jsonify,
+    request
+)
 from app.models import (
     PythonChallenge
 )
@@ -10,27 +13,27 @@ from app.Schema import (
 )
 
 # Error requests section
+
+
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
         "Message": "You haven't authorize to access this url"
-        })
+    })
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
     return jsonify({
         "Message": "Sorry, that method isn't allowed"
-        })
+    })
+
 
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
         "Message": f"Url Not found"
-        })
-
-
-
-
+    })
 
 
 @app.route("/", methods=["GET"])
@@ -43,4 +46,35 @@ def index():
 
 @app.route("/get_currency/<location>/", methods=["GET"])
 def query_currency(location: str):
-    pass
+    """[summary]
+    Args:
+        location (str): [datetime for the current currency]
+    """
+    data = PythonChallenge.query.all()
+    elements = challenge_schemas.dump(data)
+    print(request.json)
+
+    for x in elements:
+        if x["datetime"] == location:
+            
+            if request.json["currency_type"] == "chilean_peso":
+                return jsonify({
+                    "message": "sucessfully", 
+                    "status_code":200,
+                    "response": round(float(request.json["money"])*float(x["chilean_currency"]),3)
+                })
+            elif request.json["currency_type"] == "peruvian_sun":
+                return jsonify({
+                    "message": "sucessfully", 
+                    "status_code":200,
+                    "response": round(float(request.json["money"])*float(x["peruvian_currency"]),3)
+                })
+            elif request.json["currency_type"] == "euro":
+                return jsonify({
+                    "message": "sucessfully", 
+                    "status_code":200,
+                    "response": round(float(request.json["money"])*float(x["euro_currency"]),3)
+                })
+            else:
+                return jsonify({"message": "bad format"})
+    # print(elements[location])
