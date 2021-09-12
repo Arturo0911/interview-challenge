77048f49b4e1f5e577b40d7f2d1d4e552c3bb1cb
Payload <anegreiross@outlook.com>
2021-09-11 22:25:35 -0500

    Finished implementation to fetch the currency

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python
 
-from typing import Any
-from bs4 import BeautifulSoup
+"""
+@author Arturo Negreiros Samanez
+This code base was creted to automatice fetch the current change
+and store in database
+"""
+
+import pandas as pd
+from datetime import (
+    datetime,
+    timedelta,
+    date
+)
+import pandas_datareader as pdr
 from pprint import pprint
-import requests
-
-URL_HOST = "https://finance.yahoo.com/currency-converter"
-FATHER_SHAPE = "responsive-layout-portrait"
 
 
-def parsing_html(url_text: Any):
-    parsed = BeautifulSoup(url_text, 'html.parser')
+def get_info(**kwargs) -> pd.DataFrame:
+    """The arguments would be currency, time start and end date"""
+    try:
 
-    pprint(parsed.find("table").find("tbody").find_all("tr"))
-    # pprint(parsed.find_all(attrs={"class": "responsive-layout-portrait"}))
+        eur = pdr.DataReader(kwargs["currency"]+'=X', 'yahoo', start=kwargs["start_date"], end=kwargs["end_date"])['Adj Close']
+        parsed = pd.DataFrame(eur)
+        dates = [pd.to_datetime(str(x)).strftime('%Y.%m.%d') for x in eur.index.to_numpy()]
+        currencys = [x[0] for x in parsed.to_numpy()]
+
+        return pd.DataFrame({"datetime": dates, "currencys": currencys})
+    except Exception:
+        return
 
 
 def main():
-    res = requests.get(url=URL_HOST)
-    parsing_html(res.text)
+
+    print(get_info(currency="CLPUSD", start_date=datetime(2021, 9, 6), end_date=datetime(2021, 9, 11)))
 
 
 if __name__ == "__main__":
