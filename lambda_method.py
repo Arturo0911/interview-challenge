131d4d39bb7710a51e4a49c0bd1c1a7f60d803ae
Payload <anegreiross@outlook.com>
2021-09-12 22:49:53 -0500

    fetched ok lambda function

--- lambda_method.py
+++ lambda_method.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python
 
-
-import unittest
+from datetime import datetime
 import requests
 import time
 from pprint import pprint
 import dweepy
-# from bs4 import BeautifulSoup as beauty
-# from selenium import webdriver
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    Float,
+    DateTime,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+mysqlconnector://payload_root:@localhost/PythonChallenge2')
+Base = declarative_base()
+
+
+class PythonChallenge2(Base):
+    __tablename__ = "weather_data"
+
+    index = Column(Integer(), primary_key=True)
+    created_at = Column(DateTime(), default=datetime.now())
+    temperature = Column(Float(), nullable=False, unique=True)
+    humidity = Column(Float(), nullable=False, unique=True)
+
+
+    def __str__(self):
+        return self.index
+session_maker = sessionmaker(engine)
+session = session_maker()
+
 
 THING_NAME = "thecore"
 URL_REQUEST = "https://dweet.io/follow/thecore"
@@ -15,8 +40,22 @@ URL_REQUEST = "https://dweet.io/follow/thecore"
 
 def get_weather_data():
 
-    time.sleep(60)
-    print([ [x["content"]["temperature"],x["content"]["humidity"]] for x in ((dweepy.get_dweets_for(THING_NAME)))])
+    # time.sleep(60)
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    fetched = dweepy.get_dweets_for(THING_NAME)
+
+    lambda_x = lambda fetch: {
+        "temperature": round(sum([x["content"]["temperature"] for x in fetch])/len([x["content"]["temperature"] for x in fetch]), 2),
+        "humidity":round(sum([x["content"]["humidity"] for x in fetch])/len([x["content"]["humidity"] for x in fetch]), 2)
+        }; time.sleep(1)
+    print(lambda_x(fetched))
+    # humidity = [x["content"]["humidity"] for x in ((dweepy.get_dweets_for(THING_NAME)))]
+
+    # "humidity":[x["content"]["humidity"]]
+
+    #print(weather)
+    #print(humidity)
 
     # for x in dweepy.get_dweets_for(THING_NAME):
     #     print(x["content"]["temperature"])
